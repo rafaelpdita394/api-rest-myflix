ad6060049f0c99108ad6e960dbf3f6c6825445da
rafaelpdita394 <166727681+rafaelpdita394@users.noreply.github.com>
2026-03-01 09:56:06 -0300

    att

--- myflix/admin.py
+++ myflix/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from myflix.models import user, stream
 
-# Register your models here.
+class users(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'email', 'cpf', 'data_nascimento', 'celular')
+    list_display_links = ('id','nome',)
+    list_per_page = 20
+    search_fields = ('nome',)
+
+admin.site.register(user,users)
+
+class streams(admin.ModelAdmin):
+    list_display = ('id', 'codigo', 'descricao')
+    list_display_links = ('id', 'codigo', 'descricao')
+    search_fields = ('codigo',)
+
+admin.site.register(stream,streams)
--- myflix/models.py
+++ myflix/models.py
@@ -1,45 +1,24 @@
 from django.db import models
 
-# Create your models here.
-
-
-'''
-Model
-	id
-	nome
-	email
-	cpf (max 11 caracteres)
-	data de nascimento
-	numero de telefone (max 14 caracteres)
-'''
-
-'''
-
-'''
-
 class user(models.Model):
-    nome = models.CharField(max_length=255)
-    email = models.EmailField(blank= False, max_length=100)
-    cpf = models.CharField(max_length=11)
+    nome = models.CharField(max_length = 100)
+    email = models.EmailField(blank=False, max_length=30)
+    cpf = models.CharField(max_length = 11)
     data_nascimento = models.DateField()
-    numero_telefone = models.CharField(max_length=14)
+    celular = models.CharField(max_length = 14)
 
     def __str__(self):
         return self.nome
 
-'''
-    id
-	codigo do streams (max 10car )
-	descrição
-	categoria (Filmes, Série e documentário)
-'''
-
 class stream(models.Model):
-    codigo_stream = models.CharField(max_length=10)
-    descricao = models.TextField(max_length=255, blank=False)
-    categoria = models.CharField(max_length=1, choices=[('F', 'Filmes'),
-    ('S', 'Série'), ('D', 'Documentário')], blank=False, default='F')
+    CATEGORIA = (
+        ('F', 'Filme'),
+        ('S', 'Série'),
+        ('D', 'Documentário')
+    )
+    codigo = models.CharField(max_length = 10)
+    descricao = models.CharField(max_length = 100, blank=False)
+    categoria = models.CharField(max_length=1, choices=CATEGORIA, blank=False, default='F')
 
     def __str__(self):
-        return self.codigo_stream
-    
+        return self.codigo
